f3f4cde4313c739b3f0f7d6357e0bb6708e801d0
Germain <germain@sauropodstudio.com>
2017-06-04 19:10:47 -0400

    check functionalityy

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ post '/gateway' do
   message = params[:text].gsub(params[:trigger_word], '').strip
 
   action, repo = message.split('_').map {|c| c.strip.downcase }
-  repo_url = "https://api.github.com/repos/#{repo}"
+  repo_url = "https://api.github.com/Sauropod-Studio/castlestory-game"
 
   case action
     when 'issues'
       resp = HTTParty.get(repo_url)
       resp = JSON.parse resp.body
-      respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+      respond_message "There are #{resp['open_issues_count']} open issues on castlestory-game"
   end
 end
 
