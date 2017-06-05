ef9459e7e19bf3d3b38f64a04593e5d12469ec32
Germain <germain@sauropodstudio.com>
2017-06-05 01:58:24 -0400

    reverting attachement changes
    was a bad idea

--- app.rb
+++ app.rb
@@ -34,5 +34,10 @@ end
 
 def respond_message message
   content_type :json
+  {:text => message}.to_json
+end
+
+def respond_attachment message
+  content_type :json
   {:attachments => {:text => message} }}.to_json
 end
