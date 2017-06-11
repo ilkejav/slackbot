6c78332d538ed16415d282a702b88aae3e61b0da
Germain <germain@sauropodstudio.com>
2017-06-11 16:27:43 -0400

    trying attachements with random stuff

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -2,7 +2,20 @@ module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
       command 'calculate' do |client, data, _match|
-        client.say(channel: data.channel, text: '4')
+        client.say(
+        	channel: data.channel, 
+        	text: '4',
+        	attachments: [
+        		{
+		            fallback: "New ticket from Andrea Lee - Ticket #1943: Can't rest my password - https://groove.hq/path/to/ticket/1943",
+		            pretext: "New ticket from Andrea Lee",
+		            title: "Ticket #1943: Can't reset my password",
+		            title_link: "https://groove.hq/path/to/ticket/1943",
+		            text: "Help! I tried to reset my password but nothing happened!",
+		            color: "#7CD197"
+        		}
+    		].to_json
+    	)
       end
     end
   end
