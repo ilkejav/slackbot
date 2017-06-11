74c8ece8245ee3b6554f7f3452f43629daa6ff08
Germain <germain@sauropodstudio.com>
2017-06-11 17:03:40 -0400

    attempt at attachment 3

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -2,22 +2,18 @@ module SlackMathbot
   module Commands
     class Calculate < SlackRubyBot::Commands::Base
       command 'calculate' do |client, data, _match|
-        client.say(
-        	channel: data.channel, 
-        	text: "5 \n second line"
-        	,
-        	as_user: true,
-        	attachments: [
-        		{
-		            fallback: "New ticket from Andrea Lee - Ticket #1943: Can't rest my password - https://groove.hq/path/to/ticket/1943",
-		            pretext: "New ticket from Andrea Lee",
-		            title: "Ticket #1943: Can't reset my password",
-		            title_link: "https://groove.hq/path/to/ticket/1943",
-		            text: "Help! I tried to reset my password but nothing happened!",
-		            color: "#7CD197"
-        		}
-    		].to_json
-    	)
+        client.chat_postMessage(
+        channel: data.channel,
+        as_user: true,
+        attachments: [
+          {
+            fallback: "blargh blargh blargh",
+            title: "wort wort wort",
+            text: "bwarg bwarg bwarg bwag bwag",
+            color: '#00FF00'
+          }
+        ]
+      )
       end
     end
   end
