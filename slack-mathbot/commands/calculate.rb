35a4228a28cbf7361499fa44bb2ee4f058eb8f7d
Germain <germain@sauropodstudio.com>
2017-06-11 16:51:53 -0400

    attempting to_json again

--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -5,18 +5,18 @@ module SlackMathbot
         client.say(
         	channel: data.channel, 
         	text: "5 \n second line"
-      #   	,
-      #   	as_user: true,
-      #   	attachments: [
-      #   		{
-		    #         fallback: "New ticket from Andrea Lee - Ticket #1943: Can't rest my password - https://groove.hq/path/to/ticket/1943",
-		    #         pretext: "New ticket from Andrea Lee",
-		    #         title: "Ticket #1943: Can't reset my password",
-		    #         title_link: "https://groove.hq/path/to/ticket/1943",
-		    #         text: "Help! I tried to reset my password but nothing happened!",
-		    #         color: "#7CD197"
-      #   		}
-    		# ].to_json
+        	,
+        	as_user: true,
+        	attachments: [
+        		{
+		            fallback: "New ticket from Andrea Lee - Ticket #1943: Can't rest my password - https://groove.hq/path/to/ticket/1943",
+		            pretext: "New ticket from Andrea Lee",
+		            title: "Ticket #1943: Can't reset my password",
+		            title_link: "https://groove.hq/path/to/ticket/1943",
+		            text: "Help! I tried to reset my password but nothing happened!",
+		            color: "#7CD197"
+        		}
+    		].to_json
     	)
       end
     end
